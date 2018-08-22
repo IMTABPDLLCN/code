1f5b58b09b0e8fcbeb90237a0841dd713d00be57
Basile <38206582+Baz0510@users.noreply.github.com>
2018-08-22 15:36:36 +0200

    Add files via upload

---
+++ lib/Utils/text_affichage.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class TextAffichage extends StatelessWidget {
+
+  final Color color;
+  final String text;
+  final double size;
+
+  TextAffichage(this.color, this.text, this.size);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Material(
+      color: color,
+      borderRadius: BorderRadius.circular(45.0),
+      child: new Text(
+        text,
+        style: TextStyle(
+            color: Colors.white,
+            fontSize: size,
+            fontStyle: FontStyle.italic,
+            fontWeight: FontWeight.bold),
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+}
--- lib/pages/accueil_page.dart
+++ lib/pages/accueil_page.dart
@@ -3,21 +3,25 @@ import 'package:flutter/material.dart';
 import './identification_page.dart';
 
 import '../Utils/cust_bar.dart';
+import '../Utils/text_affichage.dart';
 
 class AccueilPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return new Scaffold(
+        //Main de la Page
         appBar: PreferredSize(
-        preferredSize: new Size(20.0, 60.0),
-        child: new CustBar(),
-      ),
+          preferredSize: new Size(20.0, 60.0),
+          child: new CustBar(),
+        ),
+        //Barre de l'application
         body: new Container(
           decoration: BoxDecoration(
               image: DecorationImage(
                   fit: BoxFit.cover,
                   image: AssetImage('assets/images/Image.jpeg'))),
+          //Fond d'ecran de l'application
           child: new InkWell(
             onTap: () => Navigator.of(context).push(new MaterialPageRoute(
                 builder: (BuildContext context) => new IdentificationPage())),
@@ -31,32 +35,10 @@ class AccueilPage extends StatelessWidget {
                     width: 92.0,
                   ),
                 ),
-                new Material(
-                  color: Colors.purpleAccent,
-                  borderRadius: BorderRadius.circular(45.0),
-                  child: new Text(
-                    "Hi, welcome of on the TravelBuddy world !",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 30.0,
-                        fontStyle: FontStyle.italic,
-                        fontWeight: FontWeight.bold),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-                new Material( 
-                  color: Colors.redAccent,
-                  borderRadius: BorderRadius.circular(45.0),
-                  child:new Text(
-                  " "+" Press to start ..."+" ",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 30.0,
-                      fontStyle: FontStyle.italic,
-                      fontWeight: FontWeight.bold),
-                  textAlign: TextAlign.center,
-                ) ,)
-                
+                new TextAffichage(Colors.purpleAccent,
+                    "Hi, welcome on the TravelBuddy world", 30.0),
+                new TextAffichage(
+                    Colors.redAccent, " " + " Press to start " + " ", 30.0),
               ],
             ),
           ),
